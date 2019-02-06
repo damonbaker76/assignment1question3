70abb2cd82d26e9b2e1aa92915588c9177ba240d
Damon Baker <damonbaker@Damons-MacBook-Pro.local>
2019-02-06 18:54:04 -0500

    Initial commit

---
+++ src/example/question3.java
@@ -0,0 +1,37 @@
+package example;
+import java.util.Scanner;
+public class question3 {
+//
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            int x;
+            int y;
+            int result;
+            int plus;
+            int minus;
+            int divide;
+
+            System.out.print("Enter first integer: ");
+
+            x = input.nextInt();
+
+            System.out.print("Enter second integer: ");
+
+            4y = input.nextInt();
+
+            result = x*y;
+            plus = x+y;
+            minus = y-x;
+            divide = y/x;
+
+            System.out.printf("Sum is %d%n", plus);
+                    System.out.printf("Difference is %d%n", minus);
+                            System.out.printf("Quotient is %d%n", divide);
+                                    System.out.printf("Product is %d%n", result);
+
+
+        }
+    }
+
